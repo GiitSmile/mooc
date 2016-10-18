255ae0c39bcaa48de9f54efdf29fa41189313601
xian.wang@newtouch.cn <xian.wang@newtouch.cn>
2016-10-18 10:59:35 +0800

    animation

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by LiMuyang on 2016/10/18.
+ */
+$(document).ready(function () {
+    $(".search-input").focus(function () {
+
+        $(".search-area-result").stop().slideDown(200);
+    });
+    $(".search-input").blur(function () {
+
+        $(".search-area-result").stop().slideUp(200);
+    });
+})
